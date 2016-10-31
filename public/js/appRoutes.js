eae4c259d901359c1a229636ff290470258fa4d5
Fraham <frahamp@gmail.com>
2016-10-31 20:57:07 +0000

    hello world

---
+++ public/js/appRoutes.js
@@ -0,0 +1,9 @@
+angular.module('appRoutes', [])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainController'
+            })
+    }]);
